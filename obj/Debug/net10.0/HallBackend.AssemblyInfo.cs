3e0824583fd5c21da975129bbb0c2247a1fe72f7
Arif4651 <knowldge465109@gmail.com>
2026-06-15 21:04:56 +0600

    new 2

--- obj/Debug/net10.0/HallBackend.AssemblyInfo.cs
+++ obj/Debug/net10.0/HallBackend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HallBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ef33f0200d20fae015e324e251b4852284a0ab2e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f388cbd466a8c78b9611e5f65d68c08f2310275")]
 [assembly: System.Reflection.AssemblyProductAttribute("HallBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HallBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
